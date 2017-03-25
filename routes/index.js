0ad693bb80b81d145df1a75af0281d16f8c31ebe
shubam123 <shubamsrmite@gmail.com>
2017-03-26 05:22:30 +0530

    2 chg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({"hj":"ygy"});
+  res.json({"j":"y----gy"});
 });
 
 module.exports = router;
