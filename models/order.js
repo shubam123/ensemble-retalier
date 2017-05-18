71d094e6bcaf20ced0152ff2358f14565e1c97e6
shubam123 <shubamsrmite@gmail.com>
2017-05-18 15:48:51 +0530

    order api updated

--- models/order.js
+++ models/order.js
@@ -2,45 +2,78 @@ var mongoose = require('mongoose');
 
 var Order = mongoose.model('order', {
 
-  // customer:{
-  //   name: {
-  //     type: String,
-  //     required: true
-  //   },
-  //   phone: {
-  //     type: Number,
-  //     required:true
-  //   },
-  //   email: {
-  //     type: String
-  //   },
-  //   address: {
-  //     type:String,
-  //     required:true
-  //   }
-  // },
-
-
-  // invoice:{
-  //   cost: {
-  //     type: Number,
-  //     required: true
-  //   },
-  //   products: {
-  //     type: Array,
-  //     required: true
-  //   }
-  // },
-
   customer:{
-    type: Array,
+    name: {
+      type: String,
+      required: true
+    },
+    pno: {
+      type: Number,
+      required:true
+    },
+    pno_alt: {
+      type: Number
+    },
+    email: {
+      type: String
+    },
+    address: {
+      type:String,
+      required:true
+    }
   },
-  retailer:{
-    type: Array
-  },
+
+
   invoice:{
-    type: Array
+    tot_cost: {
+      type: Number,
+      required: true
+    },
+    prods: {
+      type: Array,
+      required: true
+    }
   },
+
+  retailer:{
+      _id: {
+        type: String
+      },
+      shop_reg_id:{
+        type: String
+      },
+      shop_address:{
+        type: String
+      },
+      sales_rep_id:{
+        type:String
+      },
+      phone:{
+        type:Number
+      },
+      home_address:{
+        type:String
+      },
+      email:{
+        type:String
+      },
+      adhaar_id:{
+        type:String
+      }
+  },
+
+
+
+
+  // customer:{
+  //   type: Array,
+  // },
+  // retailer:{
+  //   type: Array
+  // },
+  // invoice:{
+  //   type: Array
+  // },
   placedAt: {
     type: String,
     default: null
